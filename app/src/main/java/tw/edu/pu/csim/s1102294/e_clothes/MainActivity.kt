9577c688da406ee227a0cc21c4bd67da88575299
LYX <s1102294@gm.pu.edu.tw>
2023-11-16 14:01:16 +0800

    logo跳至登入頁面

---
+++ app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/MainActivity.kt
@@ -0,0 +1,27 @@
+package tw.edu.pu.csim.s1102294.e_clothes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userUid = intent.getStringExtra("userUid")
+
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                val intent1 = Intent(this@MainActivity, login::class.java)
+                if (userUid != null) {
+                    intent1.putExtra("userUid", userUid)
+                }
+                startActivity(intent1)
+                finish()
+            }
+        }, 5000L)
+    }
+}
