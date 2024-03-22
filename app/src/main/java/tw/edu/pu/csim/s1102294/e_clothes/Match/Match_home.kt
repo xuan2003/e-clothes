79f50201a64a8bece3c510708a59078ea93c7a9b
LYX <s1102294@gm.pu.edu.tw>
2024-03-22 21:17:27 +0800

    登入、註冊、忘記密碼、首頁(一點)、搭配首頁(一點)

---
+++ app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/Match/Match_home.kt
@@ -0,0 +1,13 @@
+package tw.edu.pu.csim.s1102294.e_clothes.Match
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tw.edu.pu.csim.s1102294.e_clothes.R
+
+class Match_home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_home)
+
+    }
+}
--- app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/home.kt
+++ app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/home.kt
@@ -1,11 +1,29 @@
 package tw.edu.pu.csim.s1102294.e_clothes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+import tw.edu.pu.csim.s1102294.e_clothes.Match.Match_home
 
 class home : AppCompatActivity() {
+    lateinit var Match: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val userUid = intent.getStringExtra("userUid")
+
+        Match = findViewById(R.id.Match)
+        Match.setOnClickListener {
+//            Match.text = ""
+            val intent1 = Intent(this, Match_home::class.java)
+            if (userUid != null) {
+                intent1.putExtra("userUid", userUid)
+            }
+            startActivity(intent1)
+            finish()
+        }
     }
 }
