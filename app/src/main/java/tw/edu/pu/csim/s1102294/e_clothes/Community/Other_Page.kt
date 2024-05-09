e9f56d3ffc450ed3014a9930baee4fe175f25af9
LYX <s1102294@gm.pu.edu.tw>
2024-05-09 19:43:10 +0800

    登入、註冊、忘記密碼、首頁(一點)、搭配首頁(一點)、新搭配、分享搭配、好友

--- app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/Community/Other_Page.kt
+++ app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/Community/Other_Page.kt
@@ -1,12 +1,66 @@
 package tw.edu.pu.csim.s1102294.e_clothes.Community
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import tw.edu.pu.csim.s1102294.e_clothes.Match.Match_home
 import tw.edu.pu.csim.s1102294.e_clothes.R
+import tw.edu.pu.csim.s1102294.e_clothes.home
 
 class Other_Page : AppCompatActivity() {
+
+    lateinit var add_friend: Button
+    lateinit var block_account: Button
+    lateinit var Match: ImageView
+    lateinit var New_Clothes: ImageView
+    lateinit var Home: ImageView
+    lateinit var Friend: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other_page)
+
+        add_friend = findViewById(R.id.add_friend)
+        add_friend.setOnClickListener {
+            Toast.makeText(this, "已寄出好友邀請", Toast.LENGTH_SHORT).show()
+        }
+
+        block_account = findViewById(R.id.block_account)
+        block_account.setOnClickListener {
+            Toast.makeText(this, "已成功封鎖", Toast.LENGTH_SHORT).show()
+        }
+
+        Home = findViewById(R.id.Home)
+        Home.setOnClickListener {
+//            Home.text = ""
+            val intent1 = Intent(this, home::class.java)
+            startActivity(intent1)
+            finish()
+        }
+
+        Match = findViewById(R.id.Match)
+        Match.setOnClickListener {
+//            Match.text = ""
+            val intent2 = Intent(this, Match_home::class.java)
+            startActivity(intent2)
+            finish()
+        }
+
+        New_Clothes = findViewById(R.id.New_clothes)
+        New_Clothes.setOnClickListener {
+            val intent3 = Intent(this, New_Clothes::class.java)
+            startActivity(intent3)
+            finish()
+        }
+
+        Friend = findViewById(R.id.Friend)
+        Friend.setOnClickListener {
+            val intent1 = Intent(this, Friends::class.java)
+            startActivity(intent1)
+            finish()
+        }
     }
 }
