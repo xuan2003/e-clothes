65f8c3a89f3a12db5b7e730075a9502c272c7428
LYX <s1102294@gm.pu.edu.tw>
2024-05-11 18:47:45 +0800

    登入、註冊、忘記密碼、首頁(一點)、搭配首頁(一點)、新搭配、分享搭配、好友、新增衣服

--- app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/clothes/New_clothes.kt
+++ app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/clothes/New_clothes.kt
@@ -1,12 +1,50 @@
 package tw.edu.pu.csim.s1102294.e_clothes.clothes
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import tw.edu.pu.csim.s1102294.e_clothes.Match.Match_home
+import tw.edu.pu.csim.s1102294.e_clothes.Match.New_Match
 import tw.edu.pu.csim.s1102294.e_clothes.R
+import tw.edu.pu.csim.s1102294.e_clothes.home
 
 class New_clothes : AppCompatActivity() {
+
+    lateinit var finish: ImageView
+    lateinit var previous: ImageView
+    lateinit var clothes: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_clothes)
+
+
+        clothes = findViewById(R.id.clothes)
+
+        val imageBitmap = intent.getParcelableExtra<Bitmap>("capturedPhoto") // 从 Intent 中获取 Bitmap 对象
+
+        clothes.setImageBitmap(imageBitmap)
+
+        finish = findViewById(R.id.finish)
+        finish.setOnClickListener {
+            Toast.makeText(this, "搭配完成", Toast.LENGTH_SHORT).show()
+            val intent1 = Intent(this, home::class.java)
+            startActivity(intent1)
+            finish()
+
+        }
+        previous = findViewById(R.id.previous)
+        previous.setOnClickListener {
+            val intent1 = Intent(this, New_Match::class.java)
+            startActivity(intent1)
+            finish()
+        }
     }
 }
--- app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/home.kt
+++ app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/home.kt
@@ -1,10 +1,13 @@
 package tw.edu.pu.csim.s1102294.e_clothes
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import tw.edu.pu.csim.s1102294.e_clothes.Community.Friends
 import tw.edu.pu.csim.s1102294.e_clothes.Community.Personal_Page
 import tw.edu.pu.csim.s1102294.e_clothes.Match.Match_home
@@ -16,12 +19,32 @@ class home : AppCompatActivity() {
     lateinit var Friend: ImageView
     lateinit var Clothes: ImageView
     lateinit var Personal_page: ImageView
-    lateinit var textView9: TextView
+
+    private val takePictureResult =
+        registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+            val intent = Intent(this, New_clothes::class.java)
+            intent.putExtra("capturedPhoto", bitmap) // 传递 Bitmap 对象而不是资源标识符
+            startActivity(intent)
+            finish()
+        }
+
+    private val permissionsResultCallback = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()){
+        when (it) {
+            true -> {
+//                txv.text = "您允許拍照權限，歡迎使用拍照功能！"
+                takePictureResult.launch(null)
+            }
+            false -> {
+//                txv.text = "抱歉，您尚未允許拍照權限，無法使用相機功能。"
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        textView9 = findViewById(R.id.textView9)
         Home = findViewById(R.id.Home)
         Home.setOnClickListener {
 //            Home.text = ""
@@ -41,9 +64,7 @@ class home : AppCompatActivity() {
         Clothes = findViewById(R.id.Clothes)
         Clothes.setOnClickListener {
 //            textView9.text = "123"
-            val intent3 = Intent(this, New_clothes::class.java)
-            startActivity(intent3)
-            finish()
+            checkPermission()
         }
 
         Friend = findViewById(R.id.Friend)
@@ -61,4 +82,16 @@ class home : AppCompatActivity() {
         }
 
     }
+
+    private fun checkPermission() {
+        val permission = ContextCompat.checkSelfPermission(
+            this, android.Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            permissionsResultCallback.launch(android.Manifest.permission.CAMERA)
+        } else {
+//            txv.text = "您先前已允許拍照權限，歡迎使用拍照功能！"
+            takePictureResult.launch(null)
+        }
+    }
+
 }
