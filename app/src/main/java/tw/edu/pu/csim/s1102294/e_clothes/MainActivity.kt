c5815f32c9e3727678a94741033022d5618ac920
LYX <s1102294@gm.pu.edu.tw>
2024-10-27 21:34:42 +0800

    登入(V)、註冊(V)、忘記密碼(V)、修改密碼(V)、首頁[天氣(包含定位)]、搭配首頁、新搭配、分享搭配、好友、新增衣服(完成)、基本資料可上傳至firebase、相簿、相機、衣櫃(V)、單項管理頁面(V)、編輯標籤、個人頁面、新增好友(搜尋、發送好友邀請)、好友邀請列表(V)、刪除好友(有bug)、刪除帳號、新搭配頁面

--- app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1102294/e_clothes/MainActivity.kt
@@ -3,13 +3,23 @@ package tw.edu.pu.csim.s1102294.e_clothes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import coil.decode.GifDecoder
+import coil.load
 import java.util.*
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val imageView: ImageView = findViewById(R.id.imageView)
+//        imageView.load("file:///android_asset/animate_logo_gif.gif")
+        Glide.with(this)
+            .asGif() // 確保告訴 Glide 您要加載的是 GIF
+            .load(R.drawable.animate_logo_gif) // 替換為您的 GIF 資源名稱
+            .into(imageView)
         val timer = Timer()
         timer.schedule(object : TimerTask() {
             override fun run() {
